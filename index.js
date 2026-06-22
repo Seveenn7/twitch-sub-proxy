ff6974caa82e49a323ffffd33fc06242a475d0c3
Seveenn7 <tom.cordier2511@gmail.com>
2026-06-22 08:34:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,26 @@ const fetch = require('node-fetch');
 const app = express();
 
 const CLIENT_ID = '0bozrbw6s8sb3bd0w1fx0x22hl4tsq';
-let ACCESS_TOKEN = 'adg1pnbmjwtv8gs61nmkxrbohlvqpj';
+const CLIENT_SECRET = '1gkhsmc22zkjaeovl4hno0urfhb6x9';
+let REFRESH_TOKEN = 'w2orkcxefzn2dmt6ozpvqk7aybmehq4uh48oq6v6ekpuzycqt6';
+let ACCESS_TOKEN = 'au4rcj1frtn5ttqbt2au1wmkw8b3sv';
+
+async function refreshAccessToken() {
+    const res = await fetch('https://id.twitch.tv/oauth2/token', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: 'grant_type=refresh_token&refresh_token=' + REFRESH_TOKEN + '&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+    });
+    const data = await res.json();
+    if (data.access_token) {
+        ACCESS_TOKEN = data.access_token;
+        REFRESH_TOKEN = data.refresh_token;
+        console.log('Token rafraichi avec succes');
+    }
+}
+
+// Rafraichit le token toutes les 3 heures automatiquement
+setInterval(refreshAccessToken, 3 * 60 * 60 * 1000);
 
 app.get('/subs', async (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -18,12 +37,23 @@ app.get('/subs', async (req, res) => {
         const userData = await userRes.json();
         const broadcasterId = userData.data[0].id;
 
-        const subRes = await fetch('https://api.twitch.tv/helix/subscriptions?broadcaster_id=' + broadcasterId, {
+        let subRes = await fetch('https://api.twitch.tv/helix/subscriptions?broadcaster_id=' + broadcasterId, {
             headers: {
                 'Authorization': 'Bearer ' + ACCESS_TOKEN,
                 'Client-Id': CLIENT_ID
             }
         });
+
+        if (subRes.status === 401) {
+            await refreshAccessToken();
+            subRes = await fetch('https://api.twitch.tv/helix/subscriptions?broadcaster_id=' + broadcasterId, {
+                headers: {
+                    'Authorization': 'Bearer ' + ACCESS_TOKEN,
+                    'Client-Id': CLIENT_ID
+                }
+            });
+        }
+
         const subData = await subRes.json();
         res.json({ count: subData.total });
     } catch (e) {
